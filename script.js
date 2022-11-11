d676017e44bd9bdc550b83789634efccb8b8d515
elameen37 <elameen37@gmail.com>
2022-11-11 18:21:38 +0100

    update files

--- script.js
+++ script.js
@@ -14,5 +14,13 @@ buttons.forEach(button => {
 
     slides.children[newIndex].dataset.active = true
     delete activeSlide.dataset.active
+  
+    // for (i = 0; i < dots.length; i++) {
+    //   dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    // slides[slideIndex-1].style.display = "block";
+    // dots[slideIndex-1].className += " active";
+
   })
+  
 })
